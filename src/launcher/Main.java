f5c8b059fadc117f13bfe7c96eb6f42831c4797e
KCrazyMofo <khrin_yeray@hotmail.com>
2024-02-08 13:34:42 +0100

    Creacion de Main

---
+++ src/launcher/Main.java
@@ -0,0 +1,28 @@
+package launcher;
+
+
+import java.io.IOException;
+import javax.swing.SwingUtilities;
+
+public class Main {
+	
+	//private static Controller control;
+	
+	private static void startGUIMode(/*Controller control*/) throws IOException {
+				
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				//new MainWindow();
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		
+		//control = new Controller(new MainWindow());
+	       
+	}
+	
+	
+}
