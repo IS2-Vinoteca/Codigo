6d6f8601083acdb66c99dc9fa9583d4bdbb1465e
Khrin Yeray <92230056+KhrinYeray@users.noreply.github.com>
2024-03-05 16:55:44 +0100

    Packages creation

---
+++ src/control/Controller.java
@@ -0,0 +1,14 @@
+package control;
+
+import presentacion.MainWindow;
+
+public class Controller {
+
+	private MainWindow mainWindow;
+	
+	public Controller(MainWindow mainWindow) {
+		
+		this.mainWindow = mainWindow;
+		
+	}
+}
---
+++ src/presentacion/MainWindow.java
@@ -0,0 +1,67 @@
+package presentacion;
+
+import java.awt.Dimension;
+import java.awt.Image;
+
+import javax.swing.*;
+
+import control.Controller;
+
+
+public class MainWindow extends JFrame{
+ 
+	private static final long serialVersionUID = 1L;
+
+
+	private JLabel logo  ; 
+	private JPanel mainPanel;
+	private Controller controller;
+
+    public MainWindow() {
+    	super("Soul Agency") ; 
+    	controller = new Controller(this);
+    	initGUI() ; 
+    	System.out.printf("hol");
+    }
+   
+    
+    private void initGUI() {
+    	mainPanel = new JPanel( ) ; 
+    	mainPanel.setLayout(new BoxLayout( mainPanel, BoxLayout.Y_AXIS)) ;
+		setContentPane(mainPanel) ; 
+		
+		 logo = new  JLabel() ; 
+		ImageIcon lo = new ImageIcon("./resources/images/logo.png") ; 
+		 Image imagen = lo.getImage();
+		Image imagenRedimensionada = imagen.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+	    ImageIcon  logo_final = new ImageIcon(imagenRedimensionada);
+	    logo.setAlignmentX(CENTER_ALIGNMENT);  
+		logo.setIcon(logo_final);
+		
+   
+		mainPanel.add(logo)   ; 
+		
+		JLabel bienvenida= new JLabel("Bienvenid@, empieza a gestionar tu academia de superheroes!");
+		bienvenida.setAlignmentX(CENTER_ALIGNMENT);
+		mainPanel.add(bienvenida) ; 
+		
+		JButton menu = new JButton("Menu Principal") ; 
+		menu.addActionListener((e)->{
+			//controller.mostrarMenuWindow();
+			dispose();
+		}) ; 
+		
+		menu.setAlignmentX(CENTER_ALIGNMENT) ; 
+		
+		mainPanel.add(menu) ; 
+		
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500, 500) ) ;
+
+    	pack() ; 
+    	setLocationRelativeTo(null) ; 
+    	
+    }
+
+}
