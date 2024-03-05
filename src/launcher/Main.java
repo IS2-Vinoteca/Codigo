1ab2479c6259a0c5b9b0e9bb0369322b6d3079f0
deborarubio <derubio@ucm.es>
2024-03-05 14:05:34 +0100

    Update Main.java

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javax.swing.SwingUtilities;
 
 public class Main {
-	
+	//holi
 	//private static Controller control;
 	
 	private static void startGUIMode(/*Controller control*/) throws IOException {
